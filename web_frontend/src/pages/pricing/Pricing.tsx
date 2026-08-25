376a39a35565590b32fd56cd3ced5626dd532516
MD. RUHAN HOSSAIN <ruhanhossain795@gmail.com>
2026-08-25 15:59:40 +0600

    After fixing some font size in pricing part v_2

--- web_frontend/src/pages/pricing/Pricing.tsx
+++ web_frontend/src/pages/pricing/Pricing.tsx
@@ -4,7 +4,7 @@ import { pricingData } from "../../constant/pricing";
 
 const PrintRates = () => {
   return (
-    <div className="px-5 min-[500px]:px-7 sm:px-10 md:px-15 py-15 sm:py-20 md:py-25 lg:py-30 font-spaceG">
+    <div className="px-5 min-[500px]:px-10 md:px-15 py-15 sm:py-20 md:py-25 lg:py-30 font-spaceG">
 
       <div className="flex flex-col gap-4 items-center justify-center">
         <h4 className="text-sm sm:text-base font-bold text-blue">Printing Cost</h4>
@@ -12,7 +12,7 @@ const PrintRates = () => {
       </div>
 
       <div className="font-roboto flex max-sm:flex-col items-center justify-center gap-10 max-sm:gap-20 mt-20">
-        <div className="min-w-70  relative flex flex-col gap-7 border border-gray-300 rounded-xl shadow-lg p-5 w-1/3">
+        <div className="min-w-70 relative flex flex-col gap-7 border border-gray-300 rounded-xl shadow-lg p-5 w-1/3">
           <div className="float-page absolute -top-5 right-5 w-16 h-20 bg-white rounded-lg shadow-md p-3 flex flex-col gap-2 border border-gray-300">
             <div className="w-1/2 h-1.5 rounded bg-gray-800" />
             <div className="w-full h-1 rounded bg-gray-300" />
@@ -20,8 +20,8 @@ const PrintRates = () => {
             <div className="w-full h-1 rounded bg-gray-300" />
           </div>
 
-          <h2 className="font-bold text-[24px]">Black & White</h2>
-          <p className="text-md text-gray-700">From <span className="font-bold text-[40px] px-4 text-blue-500">{pricingData[0].price}</span> per page</p>
+          <h2 className="font-bold text-[20px] sm:text-[22px] md:text-[24px]">{pricingData[0].mode}</h2>
+          <p className="text-md text-gray-700">From <span className="font-bold text-[34px] sm:text-[36px] md:text-[40px] px-2 sm:px-3 lg:px-4 text-blue-500">{pricingData[0].price}</span> per page</p>
           <p className="text-sm text-gray-500">Report, Assignment, Notes and everyday Printing</p>
           <p className="text-green-700">Discounts grow the more you print</p>
         </div>
@@ -34,8 +34,8 @@ const PrintRates = () => {
             <div className="w-full h-1 rounded bg-pink-300" />
           </div>
 
-          <h2 className="font-bold text-[24px] text-white">Color</h2>
-          <p className="text-md text-gray-200">From <span className="font-bold text-[40px] px-4 text-white">{pricingData[1].price}</span> per page</p>
+          <h2 className="font-bold text-[20px] sm:text-[22px] md:text-[24px] text-white">{pricingData[1].mode}</h2>
+          <p className="text-md text-gray-200">From <span className="font-bold text-[34px] sm:text-[36px] md:text-[40px] px-2 sm:px-3 lg:px-4 text-white">{pricingData[1].price}</span> per page</p>
           <p className="text-sm text-gray-300">Colorful Documents and anything that needs to pop.</p>
           <p className="text-green-300">Discounts grow the more you print</p>
         </div>
